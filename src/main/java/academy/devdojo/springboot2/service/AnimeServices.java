b78c2a38573ecc8077a286fad570e6d91b667925
johnnybigoo <jotalico@icloud.com>
2022-03-04 21:54:22 -0500

    Springboot essentials #15 => Customized Exceptions

--- src/main/java/academy/devdojo/springboot2/service/AnimeServices.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeServices.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.exception.BadRequestException;
 import academy.devdojo.springboot2.mapper.AnimeMapper;
 import academy.devdojo.springboot2.repository.AnimeRepository;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
@@ -22,7 +23,7 @@ public class  AnimeServices {
 
     public Anime findByIdOrThrownBadRequestException(long id){
         return animeRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found. "));
+                .orElseThrow(() -> new BadRequestException("Anime not found. "));
 
     }
 
