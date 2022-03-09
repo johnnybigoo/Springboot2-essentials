92fb3799b7cd25c150fb731e61433a0b633a9367
johnnybigoo <jotalico@icloud.com>
2022-03-08 20:48:13 -0500

    Springboot essentials #18 => Fields validation

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -2,7 +2,12 @@ package academy.devdojo.springboot2.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message = "The anime name cannot be empty")
+    @NotNull(message = "The anime name cannot be null")
     private String name;
 }
