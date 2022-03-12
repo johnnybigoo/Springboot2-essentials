6888c8087c8a5415aa31f55d3cc72153e57a3af1
johnnybigoo <jotalico@icloud.com>
2022-03-11 20:48:28 -0500

    Springboot essentials #23 => Refactoring

---
+++ target/generated-sources/annotations/academy/devdojo/springboot2/mapper/AnimeMapperImpl.java
@@ -0,0 +1,38 @@
+package academy.devdojo.springboot2.mapper;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.requests.AnimePostRequestBody;
+import academy.devdojo.springboot2.requests.AnimePutRequestBody;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-03-11T20:47:18-0500",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.14 (Oracle Corporation)"
+)
+@Component
+public class AnimeMapperImpl extends AnimeMapper {
+
+    @Override
+    public Anime toAnime(AnimePostRequestBody animePostRequestBody) {
+        if ( animePostRequestBody == null ) {
+            return null;
+        }
+
+        Anime anime = new Anime();
+
+        return anime;
+    }
+
+    @Override
+    public Anime toAnime(AnimePutRequestBody animePutRequestBody) {
+        if ( animePutRequestBody == null ) {
+            return null;
+        }
+
+        Anime anime = new Anime();
+
+        return anime;
+    }
+}
